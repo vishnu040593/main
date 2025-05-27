c410731c39e31a7c60709d0bf451398e16aa6178
vishnu040593 <vishnugodala93@gmail.com>
2025-05-26 22:01:22 -0700

    Update ipl_matches.sql

--- Queries/Aggregations/ipl_matches.sql
+++ Queries/Aggregations/ipl_matches.sql
@@ -1,1 +1,9 @@
+-- Toss Winner vs Match Winner Summary
 
+SELECT
+  toss_winner,
+  winner as match_winner,
+  COUNT(*) AS match_count
+FROM ipl_matches
+GROUP BY toss_winner, winner
+ORDER BY match_count DESC;
