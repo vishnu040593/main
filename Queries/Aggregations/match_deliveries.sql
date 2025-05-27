0c828f7dea755d8d16aae1d1ecc8ec13a81b52e3
vishnu040593 <vishnugodala93@gmail.com>
2025-05-26 21:57:43 -0700

    Update match_deliveries.sql

--- Queries/Aggregations/match_deliveries.sql
+++ Queries/Aggregations/match_deliveries.sql
@@ -6,3 +6,24 @@ SELECT
 FROM match_deliveries
 GROUP BY match_id
 ORDER BY match_id;
+
+-- Top batsman by runs per match
+
+SELECT
+  match_id,
+  batsman,
+  SUM(batsman_runs) AS runs_scored
+FROM match_deliveries
+GROUP BY match_id, batsman
+ORDER BY match_id, runs_scored DESC;
+
+-- Wickets taken by each bowler per match
+
+SELECT
+  match_id,
+  bowler,
+  COUNT(*) AS wickets
+FROM match_deliveries
+WHERE dismissal_kind IS NOT NULL
+GROUP BY match_id, bowler
+ORDER BY match_id, wickets DESC;
