23bf2504e1f1fcc9c8ffe9410aaf342492324899
vishnu040593 <vishnugodala93@gmail.com>
2025-05-26 21:46:50 -0700

    Update match_deliveries.sql

--- Queries/data_quality_checks/match_deliveries.sql
+++ Queries/data_quality_checks/match_deliveries.sql
@@ -4,3 +4,13 @@ SELECT COUNT(*) AS unmatched_deliveries
 FROM match_deliveries d
 LEFT JOIN ipl_matches m ON d.match_id = m.id
 WHERE m.id IS NULL;
+
+-- To check Exact Duplicate Deliveries (if any)
+
+SELECT *
+FROM match_deliveries
+WHERE id NOT IN (
+  SELECT MIN(id)
+  FROM match_deliveries
+  GROUP BY match_id, inning, `over`, ball, batsman, bowler
+);
